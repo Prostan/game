e6e1667df0fb907fdaa2e7f0fd679a77031b17ff
Stan Adamchyk <stan.adadmchyk@gmail.com>
2016-07-30 15:30:10 -0700

    added redme

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-frontend-nanodegree-arcade-game
-===============================
+###Front End Web Developer Nanodegree
+##Project: Classic Arcade Game Clone
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+### Project Goals
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Learn courses:
+
+* [Object-Oriented Javascript](https://www.udacity.com/course/ud015)
+* [HTML5 Canvas](https://www.udacity.com/course/ud292)
+* [Writing READMEs](https://www.udacity.com/course/ud777)
+
+
+
+##Game design.
+
+<img src="game" width="563" height="670" />
+
+
+## Running the Game
+
+Download the game to your computer and open the index.html
+in your favorite browser.
--- js/app.js
+++ js/app.js
@@ -1,68 +1,115 @@
+//Constants
+var CANVAS_WIDTH = 505;
+var CANVAS_HEIGHT = 606;
+var ENEMY_ROWS = [0, 63, 146, 229];
+var START_Y = 396;
 
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = 200;
-    this.y = 200;
+// Enemy Constructor
+var Enemy = function () {
+  this.x = -100;
+  this.y = this.randomRow();
+  this.speed = this.randomSpeed();
+  this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+// Return random row for enemy respawn
+Enemy.prototype.randomRow = function () {
+  return ENEMY_ROWS[Math.ceil(Math.random() * 3)];
+};
+
+// Enemy random speed
+Enemy.prototype.randomSpeed = function () {
+  return Math.floor(Math.random() * (300 - 100) + 100);
+};
+
+// Respawn enemy
+Enemy.prototype.respawn = function () {
+  this.x = -100;
+  this.y = this.randomRow();
+  this.speed = this.randomSpeed();
+};
+
+// Move enemy with different speed
+Enemy.prototype.update = function (dt) {
+  this.x += (this.speed * dt);
+  //Enemy has reached the end of the row and should respown
+  if (this.x > CANVAS_WIDTH) {
+    this.respawn();
+  }
+  // Check if ememy is colliding with player
+  this.collidingWithPlayer();
+};
+
+Enemy.prototype.collidingWithPlayer = function () {
+  if (player.x <= this.x + 75 &&
+    this.x <= player.x + 75 &&
+    player.y <= this.y + 50 &&
+    this.y <= player.y + 50) {
+    // When colliding with player, respawn player
+    player.respawn();
+  }
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function() {
-  this.sprite = 'images/char-boy.png';
-  this.x = 200;
-  this.y = 400;
-};
-
-Player.prototype.update = function() {
-
-};
-
-Player.prototype.render = function() {
+Enemy.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
-
+// Player Constructor
+var Player = function () {
+  this.respawn();
+  this.sprite = 'images/char-boy.png';
 };
 
-var player = new Player();
+// Respawn player
+Player.prototype.respawn = function () {
+  this.x = 202;
+  this.y = START_Y;
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+
+// This class requires an update(), render() and
+// a handleInput() method.
+Player.prototype.update = function () {
+  // Player has reached top of canvas and has won
+  if (this.y <= 0) {
+    this.respawn();
+  }
+};
+
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//While handleInput we make sure player is not moving out of canvas
+Player.prototype.handleInput = function (keyCode) {
+  if (keyCode === 'up') { this.y -= 83; }
+  if (keyCode === 'down' && this.y < 350) { this.y += 83; }
+  if (keyCode === 'left' && this.x > 50) { this.x -= 101; }
+  if (keyCode === 'right' && this.x < 350) { this.x += 101; }
+};
+
+// Create enemy objects
+for (i = 0; i < 4; i++) {
+  allEnemies.push(new Enemy());
+}
 
 // Place the player object in a variable called player
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+// Player.handleInput() method.
+document.addEventListener('keyup', function (e) {
+  var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
